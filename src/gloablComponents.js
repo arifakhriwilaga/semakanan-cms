d2c7b38dfe868cd21ccbee4e27cd6c95490b8c96
ushop <fajar.purnama440@gmail.com>
2018-10-06 22:56:29 +0700

    assign component spinner

--- src/gloablComponents.js
+++ src/gloablComponents.js
@@ -2,6 +2,8 @@ import fgInput from './components/UIComponents/Inputs/formGroupInput.vue'
 import DropDown from './components/UIComponents/Dropdown.vue'
 import Checkbox from './components/UIComponents/Inputs/Checkbox.vue'
 import Radio from './components/UIComponents/Inputs/Radio.vue'
+import Spinner from './components/UIComponents/Spinner.vue'
+// import Switch from './components/UIComponents/Switch.vue'
 /**
  * You can register global components here and use them as a plugin in your main Vue instance
  * Ideally, only small components that are re-used many times across your application should be registered here.
@@ -14,6 +16,8 @@ const GlobalComponents = {
     Vue.component(DropDown.name, DropDown)
     Vue.component(Checkbox.name, Checkbox)
     Vue.component(Radio.name, Radio)
+    Vue.component(Spinner.name, Spinner)
+    // Vue.component(Switch.name, Spinner)
   }
 }
 
