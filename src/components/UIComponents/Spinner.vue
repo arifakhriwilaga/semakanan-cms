43589bb28ef876745b7cc7adbed123e48ab3388f
ushop <fajar.purnama440@gmail.com>
2018-10-06 22:54:34 +0700

    create spinner

---
+++ src/components/UIComponents/Spinner.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div class="overlay" style="z-index:10" v-if="showSpinner">
+      <div class="overlay__inner">
+            <div class="overlay__content">
+              <span class="spinner"></span>
+              <span class="text-spinner">Loading...</span>
+            </div>
+        </div>
+    </div>
+  </div>
+</template>
+<script>
+    
+    export default {
+        name: 'spinner',
+        props: {
+            showSpinner: false
+        }
+    }
+
+</script>
