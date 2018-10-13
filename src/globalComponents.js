18dd881650c167f69b6ea0c5c2a05cb44b859cc5
ushop <fajar.purnama440@gmail.com>
2018-10-14 06:57:00 +0700

    include vue-fontawesome

--- src/globalComponents.js
+++ src/globalComponents.js
@@ -14,6 +14,9 @@ import KalendarCell from './components/UIComponents/Kalendar/kalendar-cell';
 import KalendarDay from './components/UIComponents/Kalendar/kalendar-day';
 import KalendarEventpopup from './components/UIComponents/Kalendar/kalendar-eventpopup';
 import KalendarWeekView from './components/UIComponents/Kalendar/kalendar-weekview';
+import 'vue-awesome/icons'
+import Icon from 'vue-awesome/components/Icon'
+
 // import * from './components/UIComponents/Kalendar'
 // import Switch from './components/UIComponents/Switch.vue'
 /**
@@ -24,12 +27,12 @@ import KalendarWeekView from './components/UIComponents/Kalendar/kalendar-weekvi
 
 const GlobalComponents = {
   install (Vue) {
-    Vue.component(fgInput.name, fgInput)
-    Vue.component(DropDown.name, DropDown)
-    Vue.component(Checkbox.name, Checkbox)
-    Vue.component(Radio.name, Radio)
-    Vue.component(Spinner.name, Spinner)
-    Vue.component('show-image', ShowImage)
+    Vue.component(fgInput.name, fgInput);
+    Vue.component(DropDown.name, DropDown);
+    Vue.component(Checkbox.name, Checkbox);
+    Vue.component(Radio.name, Radio);
+    Vue.component(Spinner.name, Spinner);
+    Vue.component('show-image', ShowImage);
 
     // Kalendar
     Vue.component('kalendar', Kalendar);
@@ -37,6 +40,10 @@ const GlobalComponents = {
     Vue.component('kalendar-day', KalendarDay);
     Vue.component('kalendar-eventpopup', KalendarEventpopup);
     Vue.component('kalendar-week-view', KalendarWeekView);
+
+    // icon
+    Vue.component('v-icon', Icon);
+
     // Vue.component(Switch.name, Spinner)
   }
 }
